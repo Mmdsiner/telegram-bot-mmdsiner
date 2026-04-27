7ed35eea3443796a13501fc3fdf16b2b7072da35
Mmdsiner <mmdkingoff@gmail.com>
2026-04-27 22:34:14 +0330

    Update models.py

--- models.py
+++ models.py
@@ -18,5 +18,5 @@ class Order(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(BigInteger)
     amount = Column(Integer)
-    status = Column(String)  # pending / paid / rejected
+    status = Column(String)
     receipt = Column(String)
