1977ee87ca3d4950ea7e292e4f0e6acbff6af5b2
Mmdsiner <mmdkingoff@gmail.com>
2026-04-27 22:19:39 +0330

    Delete handlers/payment.py

--- handlers/payment.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router, F
-from aiogram.types import Message
-
-router = Router()
-
-@router.message(F.photo)
-async def receipt(message: Message):
-    await message.answer("رسید دریافت شد ✅")
