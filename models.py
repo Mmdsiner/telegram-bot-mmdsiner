4215e27b5c9e8e6528912cc5d48ae26d71aa104d
Mmdsiner <mmdkingoff@gmail.com>
2026-04-27 22:07:34 +0330

    Create models.py

---
+++ models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, BigInteger
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(BigInteger, primary_key=True)
+    balance = Column(Integer, default=0)
+    invited_by = Column(BigInteger)
+    successful_invites = Column(Integer, default=0)
+
+class Settings(Base):
+    __tablename__ = "settings"
+    key = Column(String, primary_key=True)
+    value = Column(String)
+
+class Order(Base):
+    __tablename__ = "orders"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger)
+    amount = Column(Integer)
+    status = Column(String)
