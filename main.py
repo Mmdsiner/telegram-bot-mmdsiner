62780c70f8127007ba7986551ec6fe8209f442e7
Mmdsiner <mmdkingoff@gmail.com>
2026-04-27 22:03:57 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,15 @@ import asyncio
 from aiogram import Bot, Dispatcher
 
 from config import BOT_TOKEN
-from handlers import user, admin, payment
+from handlers import user, admin
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+dp.include_router(user.router)
+dp.include_router(admin.router)
 
 async def main():
-    bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher()
-
-    dp.include_router(user.router)
-    dp.include_router(admin.router)
-    dp.include_router(payment.router)
-
-    print("Bot started...")
-
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
