c08f90e31bf0cddec484ac2285399395f107abb0
Mmdsiner <mmdkingoff@gmail.com>
2026-04-27 22:56:08 +0330

    Update database.py

--- database.py
+++ database.py
@@ -4,13 +4,19 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
+# تبدیل اجباری (حتی اگر اشتباه باشه)
+if DATABASE_URL:
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://")
 
-engine = create_async_engine(DATABASE_URL, echo=False)
+print("FINAL DB URL:", DATABASE_URL)
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False
+)
 
 SessionLocal = sessionmaker(
-    engine,
+    bind=engine,
     class_=AsyncSession,
     expire_on_commit=False
 )
