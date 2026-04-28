89c3c1d97cd0c7027c7c8b9be457f65dffae5fa7
Mmdsiner <mmdkingoff@gmail.com>
2026-04-28 18:07:54 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-
 from config import BOT_TOKEN
 from handlers import user, admin
-from database import engine, Base
-
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-
-dp.include_router(user.router)
-dp.include_router(admin.router)
-
-async def create_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
 
 async def main():
-    await create_tables()
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
+    dp.include_router(user.router)
+    dp.include_router(admin.router)
+
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
